add8e05c6416b625e8007fc4c90f4cc1e584206e
Jay Rajesh Munjapara <59328819+jay-munjapara@users.noreply.github.com>
2021-04-08 02:11:27 +0530

    Add files via upload

--- metropolitian_visualization.py
+++ metropolitian_visualization.py
@@ -1,17 +1,16 @@
-#import libraries as per man pade!!
-import pandas as pd
+########## import libraries as per man pade!! ##########################################################################
+
+import base64
+import plotly
 import numpy as np
-import plotly.graph_objects as go
+import pandas as pd
+import streamlit as st
 from plotly import tools
 import plotly.offline as py
 import plotly.express as px
-import plotly
+import plotly.graph_objects as go
 from sklearn.decomposition import PCA
-
-import streamlit as st
 import streamlit.components.v1 as components
-
-import base64
 
 ########################################################################################################################
 
@@ -20,14 +19,14 @@ import base64
 # Use the full page instead of a narrow central column
 st.set_page_config(layout="wide")
 
-st.title("**Air Quality Visualisation for Metropolitian Cities**")
-# st.write("Air quality index (AQI) along with air pollution, Health Condition and Demographic data near {}".format(city))
-# st.write("Air quality index (AQI) and PM2.5 air pollution near Colaba, Mumbai - MPCB, Uran")
+st.title("**Air Quality Visualisation (AQV)**")
+st.write("BY JAY | ISHA | JENIL | YASH")
+st.image('img/air_quality.jpg')
 
-# st.sidebar.selectbox(
-#     "You Can do the following using this Website ",
-#     ('Data Pre Processing using Pandas', 'Correcting','Completing','Creating','Modeling using Sklearn')
-# )
+########################################################################################################################
+
+st.title("**Air Quality Visualisation for Metropolitian Cities**")
+st.write("Air quality index (AQI) along with air pollution, Health Condition and Demographic data near")
 
 ########## CITY - DATA #################################################################################################
 
@@ -46,16 +45,6 @@ if st.checkbox('Show Data'):
     coded_data = base64.b64encode(df.to_csv(index = False).encode()).decode()
     st.markdown(f'<a href="data:file/csv;base64,{df}" download = "data.csv">Download Data</a>', unsafe_allow_html = True)
 
-st.write("Air quality index (AQI) along with air pollution, Health Condition and Demographic data near {}".format(city))
-
-########## 2D GRAPH PLOT ###############################################################################################
-
-fig_2d = px.scatter_mapbox(
-    df, lat="Latitude", lon="Longitude", color="AQI", size="Population",
-    color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10, 
-    hover_name = "Area", hover_data = ["Health"]
-)
-
 ########## 3D GRAPH PLOT ###############################################################################################
 
 X = df[['AQI', 'Population', 'Health', 'Latitude', 'Longitude']]
@@ -67,30 +56,51 @@ total_var = pca.explained_variance_ratio_.sum() * 100
 
 fig_3d = px.scatter_3d(
     df, x='AQI', y='Population', z='Health',
-    color='AQI', hover_name='Area',
+    color='AQI',color_continuous_scale=[[0, 'green'], [0.5, 'yellow'], [1.0, 'red']], hover_name='Area',
     hover_data=['AQI', 'Population', 'Health', 'Latitude', 'Longitude', 'Area'],
     title = f'Total Explained Variance: {total_var:.2f}%',
-    #labels = {'0': 'Population', '1': 'Sex Ratio', '2': 'Literacy'} , symbol='Population'  , 'Ward','Station'
 )
 
-########################################################################################################################
+# fig_3d.update_layout(
+#     margin=dict(l=25, r=25, t=25, b=25),
+#     paper_bgcolor="LightSteelBlue",
+# )
+
+########## 2D GRAPH PLOT ###############################################################################################
+
+fig_2d = px.scatter_mapbox(
+    df, lat="Latitude", lon="Longitude", color="AQI", size="Population",
+    color_continuous_scale=[[0, 'green'], [0.5, 'yellow'], [1.0, 'red']], 
+    size_max=15, zoom=10, hover_name = "Area", hover_data = ["Health"]
+)
+
+# fig_2d.update_layout(
+#     margin=dict(l=10, r=10, t=10, b=10),
+#     paper_bgcolor="LightSteelBlue",
+# )
 
 ########################################################################################################################
 
-c1, c2 = st.beta_columns((1.1, 1))
+########################################################################################################################
+
+c1, c2 = st.beta_columns((1.1,1))
 
 with c1:
     st.subheader('3D Graph')
     st.plotly_chart(fig_3d)
-    st.success("3D Graph Plotted!!! Yassss!!")
+    # st.success("3D Graph Plotted!!! Yassss!!")
     # st.markdown("Looks like the average publication date is around **{}**, with your oldest book being **{}** and your youngest being **{}**.".format(1,2,3))
     # st.markdown("Note that the publication date on Goodreads is the **last** publication date, so the data is altered for any book that has been republished by a publisher.")
 
 with c2:
     st.subheader('2D Graph')
     st.plotly_chart(fig_2d)
-    st.success("2D Graph Plotted!!! Yassss!!")
+    # st.success("2D Graph Plotted!!! Yassss!!")
 
 ########################################################################################################################
 
+c1, c2, c3 = st.beta_columns((1,2,1))
+with c2:
+    st.image('img/aqi.png')
+    
 ########################################################################################################################
